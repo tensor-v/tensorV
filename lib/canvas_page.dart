bdd3f5cf2560fa00469ba3040ea2810412baaba3
jisung <jisung6723@gmail.com>
2025-07-05 02:24:46 +0900

    Create CanvasPage

---
+++ lib/canvas_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class CanvasPage extends StatelessWidget {
+  const CanvasPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("Body"),
+    );
+  }
+}
